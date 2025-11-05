8cbbe1fd75d705f391611bc77352a44414e6f71a
mariag1996 <maria_gomez251996@hotmail.com>
2025-11-05 12:14:22 -0300

    Update asignatura_update.php

--- pages/asignatura_update.php
+++ pages/asignatura_update.php
@@ -1,16 +1,8 @@
 <!-- ......Este archivo se encarga de procesar el formulario de edición (asignatura_edit.php)........ -->
 <?php
-// =====================================
-//  CONEXIÓN A LA BASE DE DATOS
-// =====================================
 
 // Se incluye el archivo 'db.php', que contiene la función getDB()
-// para obtener una conexión segura a la base de datos mediante mysqli.
 require_once __DIR__ . '/db.php';
-
-// =====================================
-//  VERIFICAR MÉTODO DE ACCESO
-// =====================================
 
 //Si el script no fue accedido mediante POST (es decir, desde un formulario).
 //  Por ejemplo, si alguien lo abre directamente en el navegador
@@ -19,10 +11,6 @@ if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     header("Location: asignaturas_registro.php");
     exit;
 }
-
-// =====================================
-//  OBTENCIÓN Y VALIDACIÓN DE DATOS
-// =====================================
 
 //Se obtienen los datos enviados por el formulario
 // y se usa trim() para eliminar espacios en blanco al inicio y al final.
@@ -38,24 +26,14 @@ if ($id_asignatura <= 0 || $nombre === '') {
     die("Datos inválidos.");
 }
 
-// =====================================
-//  ACTUALIZAR ASIGNATURA EN LA BASE DE DATOS
-// =====================================
-
 try {
     // Se prepara una consulta SQL segura para actualizar los datos de la asignatura
     // Los signos ? son marcadores de posición que luego se reemplazan con los valores reales
     $stmt = getDB()->prepare("UPDATE asignatura SET nombre = ?, activo = ?  WHERE id_asignatura = ?");
     // "sii" indica los tipos de datos: string (s), integer (i), integer (i)
     $stmt->bind_param("sii", $nombre,  $activo, $id_asignatura);
-    // Se ejecuta la actualización
     $stmt->execute();
-    // Se cierra el statement para liberar recursos
     $stmt->close();
-
-    // =====================================
-    //  REDIRECCIÓN POST-ACTUALIZACIÓN
-    // =====================================
 
     // Si todo salió bien, redirige de nuevo a la página principal de asignaturas
     header("Location: asignaturas_registro.php");
